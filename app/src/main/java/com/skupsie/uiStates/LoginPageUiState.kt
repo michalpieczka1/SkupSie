8196a3014cf80b5a2ae8b95a78327f527e7ad667
michalpieczka1 <michalpieczka280@gmail.com>
2024-02-23 09:49:35 +0100

    Added ViewModel and UiState

---
+++ app/src/main/java/com/skupsie/uiStates/LoginPageUiState.kt
@@ -0,0 +1,12 @@
+package com.skupsie.uiStates
+
+data class LoginPageUiState(
+    val emailValue:String = "",
+    val passwordValue:String = "",
+    val isEmailValid:Boolean = false,
+    val isPasswordValid:Boolean = false,
+    val forgotPasswordOnClick: () -> Unit = {},
+    val loginOnClick: () -> Unit = {},
+    val registerOnClick: () -> Unit = {}
+    //TODO add google api login support
+)
---
+++ app/src/main/java/com/skupsie/viewmodels/LoginPageViewModel.kt
@@ -0,0 +1,44 @@
+package com.skupsie.viewmodels
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import com.skupsie.uiStates.LoginPageUiState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class LoginPageViewModel : ViewModel() {
+    private val _uiState = MutableStateFlow(LoginPageUiState())
+    val uiState:StateFlow<LoginPageUiState> = _uiState.asStateFlow()
+
+    fun onEmailChange(email: String){
+        _uiState.value = LoginPageUiState(emailValue = email)
+    }
+    fun onPasswordChange(password: String){
+        _uiState.value = LoginPageUiState(passwordValue = password)
+    }
+
+    fun isEmailValid(email: String) {
+        _uiState.value = LoginPageUiState(isEmailValid = email.contains('@') )
+    }
+    fun isPasswordValid(password: String) {
+        _uiState.value = LoginPageUiState(isPasswordValid = password.contains(regex =
+        Regex("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{6,}\$"))
+        )
+    }
+
+    private val testTagDeleteLater = "testttt" //TODO Delete later
+    fun forgotPasswordOnClick(){
+        //TODO NAV
+        _uiState.value = LoginPageUiState(forgotPasswordOnClick = { Log.d(testTagDeleteLater,"Forgot password clicked")})
+    }
+    fun loginOnClick(){
+        //TODO NAV
+        _uiState.value = LoginPageUiState(loginOnClick = { Log.d(testTagDeleteLater,"Login clicked")})
+    }
+    fun registerOnClick(){
+        //TODO NAV
+        _uiState.value = LoginPageUiState(registerOnClick = { Log.d(testTagDeleteLater,"Register clicked")})
+    }
+
+}
